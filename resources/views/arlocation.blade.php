4366b0e31d618705d402a7e756030cf9a8ac34e2
fatimabed <fatima.abed@softylus.com>
2023-02-12 12:33:12 +0300

    ar1

--- resources/views/arlocation.blade.php
+++ resources/views/arlocation.blade.php
@@ -31,23 +31,15 @@ use App\Models\objectt;
      
      <a-scene vr-mode-ui='enabled: false' arjs='sourceType: webcam; videoTexture: true; debugUIEnabled: false' renderer='antialias: true; alpha: true'>
        <a-camera gps-new-camera='gpsMinDistance: 5'></a-camera>
-       <a-assets><a-asset-item src="object/{{$object->object}}"></a-asset-item></a-assets>
+       <a-assets><a-asset-item id="animated-asset" src="object/{{$object->object}}"></a-asset-item></a-assets>
        <a-entity material='color: red' geometry='primitive: box' gps-new-entity-place="latitude: 32.085787 ; longitude: 36.0933741" scale="10 10 10"></a-entity>
       </a-scene>
-      <!-- <h1>{{$location->latitude}}</h1>
-     <h1>{{$location->longitude}}</h1> -->
+      
       <?php }
       
       ?>
 <!-- <a-scene embedded arjs>
       <a-marker preset="hiro"> -->
-        <!-- we use cors proxy to avoid cross-origin problems -->
-        <!--
-          ⚠️⚠️⚠️
-          https://arjs-cors-proxy.herokuapp.com/ is now offline, Heroku has dismissed all his free plans from November 2022.
-          You need to host your own proxy and use it instead. The proxy is based on CORS Anywhere (see https://github.com/Rob--W/cors-anywhere).
-          ⚠️⚠️⚠️
-        -->
         <!-- <a-entity
           position="0 0 0"
           scale="0.05 0.05 0.05"
