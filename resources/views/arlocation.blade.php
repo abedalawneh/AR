ecff3148586f4dd0ae8ea6f5bbf0e83b5045ca90
fatimabed <fatima.abed@softylus.com>
2023-02-16 14:35:10 +0300

    gltf10000

--- resources/views/arlocation.blade.php
+++ resources/views/arlocation.blade.php
@@ -37,7 +37,7 @@ use App\Models\objectt;
           look-at="[gps-entity-place]"></a-camera>
         <a-entity  position="0 0 0" scale="10 10 10" gltf-model="{{ asset($name.'/'.$object->object) }}"
         gps-new-entity-place="latitude:{{$location->latitude}}; longitude:{{ $location->longitude}}"
-        animation__rotate="property: rotation; to: 0 360 0; loop: true; dur: 20000"
+        animation__rotate="property: rotation; to: 0 360 0; loop: true; dur: 10000"
            model-controls
            ></a-entity>
         
