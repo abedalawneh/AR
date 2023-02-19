4ffc6c1764bed717cb5fa02418cea9e2dfd65a21
fatimabed <fatima.abed@softylus.com>
2023-02-19 11:48:23 +0300

    super-hands

--- resources/views/arlocation.blade.php
+++ resources/views/arlocation.blade.php
@@ -44,13 +44,13 @@ use App\Models\objectt;
   
   <a-camera gps-new-camera='gpsMinDistance: 5' raycaster></a-camera>
   
-  <a-entity id="gltfContainer"
+  <!-- <a-entity id="gltfContainer"
             position="0 0 0" scale="10 10 10"
             gltf-model="#{{ $object->object }}"
             gps-new-entity-place="latitude:{{ $location->latitude }}; longitude:{{ $location->longitude }}"
             animation__rotate="property: rotation; to: 0 360 0; loop: true; dur: 10000"
             ></a-entity>
-            
+             -->
   <a-entity id="gltfCollider"
             position="0 0 0" scale="10 10 10"
             gps-new-entity-place="latitude:{{ $location->latitude }}; longitude:{{ $location->longitude }}"
