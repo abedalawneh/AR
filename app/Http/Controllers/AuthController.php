29e22bfee90202c44d9a57d14f8894be0d70d7d5
fatimabed <fatima.abed@softylus.com>
2023-02-26 14:32:41 +0300

    project

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -26,7 +26,7 @@ class AuthController extends Controller
    
         $credentials = $request->only('email', 'password');
         if (Auth::attempt($credentials)) {
-            return redirect()->route('homeall')
+            return redirect()->route('project')
                         ->withSuccess('Signed in');
         }
   
