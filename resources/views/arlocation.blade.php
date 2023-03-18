851d03318023094eea83eb8490f508b6f84a3dae
fatimabed <fatima.abed@softylus.com>
2023-03-18 16:18:39 +0300

    test

--- resources/views/arlocation.blade.php
+++ resources/views/arlocation.blade.php
@@ -41,7 +41,7 @@ use App\Models\objectt;
             gps-new-entity-place="latitude:{{$location->latitude}}; longitude:{{ $location->longitude}}"
             animation__rotate="property: rotation; to: 0 360 0; loop: true; dur: 20000" super-hands
             geometry="primitive: sphere; radius: 1000">
-            <a-text value="{{$object->textobject}}" position="0 1 0" color="red" transparent="true"></a-text>
+            <a-text value="{{$object->textobject}}" position="0 -1 0" color="red" transparent="true"></a-text>
         </a-entity>
 
 
