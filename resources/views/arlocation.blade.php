bf10fef3e86425d010a3c97c70ec3b02259773c6
fatimabed <fatima.abed@softylus.com>
2023-03-19 10:52:45 +0300

    position

--- resources/views/arlocation.blade.php
+++ resources/views/arlocation.blade.php
@@ -34,20 +34,20 @@ use App\Models\objectt;
 
 
 
-<a-scene embedded vr-mode-ui='enabled: false' arjs='sourceType: webcam; debugUIEnabled: false;'>
+<a-scene embedded vr-mode-ui='enabled: false' arjs='sourceType: webcam; debugUIEnabled: false; '>
   <a-assets>
     <a-asset-item id="tree" src="{{ asset($name.'/'.$object->object) }}"></a-asset-item>
   </a-assets>
 
-  <a-entity gps-camera rotation-reader></a-entity>
+  <!-- <a-entity gps-camera rotation-reader></a-entity> -->
 
-  <a-entity gps-entity-place="latitude: {{ $location->latitude }}; longitude: {{ $location->longitude }};"
+  <a-entity  gps-camera rotation-reader gps-entity-place="latitude: {{ $location->latitude }}; longitude: {{ $location->longitude }};"
     position="1 1 -4" 
     gltf-model="#tree" scale="0.5 0.5 0.5"
     animation__rotate="property: rotation; to: 0 360 0; loop: true; dur: 20000" super-hands
             geometry="primitive: sphere; radius: 1000">
+            <a-text value="{{$object->textobject}}" position="0 1 0" color="red" transparent="true"></a-text>
         </a-entity>
-        <a-text value="{{$object->textobject}}" position="0 1 0" color="red" transparent="true"></a-text>
 
   <a-marker-camera preset="hiro"></a-marker-camera>
 
