bc4a78ebbca14939c26b345a2ac6c6cab2e6733d
fatimabed <fatima.abed@softylus.com>
2023-04-11 15:26:00 +0300

    test

--- resources/views/arlocation.blade.php
+++ resources/views/arlocation.blade.php
@@ -41,8 +41,8 @@ use App\Models\objectt;
 
 
   <a-entity  gps-camera rotation-reader gps-entity-place="latitude: {{ $location->latitude }}; longitude: {{ $location->longitude }};"
-    position="0 0 -10" 
-    gltf-model="#tree" scale="0.1 0.1 0.1"
+    position="0 0 -40" 
+    gltf-model="#tree" scale="0.5 0.5 0.5"
     animation__rotate="property: rotation; to: 0 360 0; loop: true; dur: 20000" super-hands
             geometry="primitive: sphere; radius: 1000">
             <a-text value="{{$object->textobject}}" position="0 1 0" color="red" transparent="true"></a-text>
