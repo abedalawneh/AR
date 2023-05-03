9cba40d42397c30b078f491515450a1c24ba76a1
fatimabed <fatima.abed@softylus.com>
2023-05-03 22:30:09 +0300

    position location

--- resources/views/arlocation.blade.php
+++ resources/views/arlocation.blade.php
@@ -74,13 +74,14 @@ import aframeExtrasAnimationMixer from 'https://cdn.jsdelivr.net/npm/aframe-extr
         <a-asset-item id="tree" src="{{ asset($name.'/'.$object->object) }}"></a-asset-item>
     </a-assets>
 
-    <a-entity id="gps-entity" gps-entity-place="latitude: {{ $location->latitude }}; longitude: {{ $location->longitude }};">
-        <a-entity id="myEntity" gltf-model="#tree" animation-mixer scale="0.5 0.5 0.5"
-            animation__rotate="property: rotation; to: 0 360 0; loop: true; dur: 20000" super-hands
-            geometry="primitive: sphere; radius: 1000" position="0 0 -4">
-            <a-text value="{{$object->textobject}}" position="0 1 0" color="red" transparent="true"></a-text>
-        </a-entity>
+    <a-entity gps-camera position="0 0 -4" rotation-reader>
+    <a-entity id="myEntity" gps-entity-place="latitude: {{ $location->latitude }}; longitude: {{ $location->longitude }}; altitude: {{ $location->altitude }};"
+        gltf-model="#tree" animation-mixer scale="0.5 0.5 0.5"
+        animation__rotate="property: rotation; to: 0 360 0; loop: true; dur: 20000" super-hands
+        geometry="primitive: sphere; radius: 1000">
+        <a-text value="{{$object->textobject}}" position="0 1 0" color="red" transparent="true"></a-text>
     </a-entity>
+</a-entity>
 
     <a-marker-camera preset="hiro"></a-marker-camera>
 
