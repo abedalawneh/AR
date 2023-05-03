373e5d758ebf611d31cee46f0e57ad36ed605508
fatimabed <fatima.abed@softylus.com>
2023-05-03 13:02:17 +0300

    ttest

--- resources/views/arlocation.blade.php
+++ resources/views/arlocation.blade.php
@@ -37,8 +37,7 @@ use App\Models\objectt;
 
 
 
-    <a-scene vr-mode-ui='enabled: false' arjs='sourceType: webcam; videoTexture: true; debugUIEnabled: false'
-        renderer='antialias: true; alpha: true'>
+<a-scene embedded vr-mode-ui='enabled: false' arjs='sourceType: webcam; debugUIEnabled: false; '>
         <a-camera position="0 0 -4" fov="100" gps-new-camera='gpsMinDistance: 5'></a-camera>
           <a-entity id="myEntity" gps-camera rotation-reader gps-entity-place="latitude: {{ $location->latitude }}; longitude: {{ $location->longitude }};"
     position="0 0 -4" 
