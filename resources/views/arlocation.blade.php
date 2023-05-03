99d1d9f24a9452a94384efa1f30dc02c7354231a
fatimabed <fatima.abed@softylus.com>
2023-05-03 16:10:29 +0300

    tttest

--- resources/views/arlocation.blade.php
+++ resources/views/arlocation.blade.php
@@ -40,34 +40,39 @@ import aframeExtrasAnimationMixer from 'https://cdn.jsdelivr.net/npm/aframe-extr
 
 
 
-  <!-- <a-scene vr-mode-ui='enabled: false' arjs='sourceType: webcam; videoTexture: true; debugUIEnabled: false' 
-  renderer='antialias: true; alpha: true'>
+  <!-- <a-scene embedded vr-mode-ui='enabled: false' arjs='sourceType: webcam; debugUIEnabled: false; '>
   <a-assets>
     <a-asset-item id="tree" src="{{ asset($name.'/'.$object->object) }}"></a-asset-item>
   </a-assets>
-  <a-camera gps-new-camera='gpsMinDistance: 5'></a-camera>
-    <a-entity id="myEntity" gps-entity-place="latitude: {{ $location->latitude }}; longitude: {{ $location->longitude }};"
-    gltf-model="#tree" position="0 0 -4" animation-mixer scale="0.5 0.5 0.5"
-      animation__rotate="property: rotation; to: 0 360 0; loop: true; dur: 20000">
-      <a-text value="{{$object->textobject}}" position="0 1 0" color="red" transparent="true"></a-text>
-    </a-entity> -->
+
+
+  <a-entity id="myEntity" gps-camera rotation-reader gps-entity-place="latitude: {{ $location->latitude }}; longitude: {{ $location->longitude }};"
+    position="0 0 -4" 
+    gltf-model="#tree" animation-mixer scale="0.5 0.5 0.5"
+    animation__rotate="property: rotation; to: 0 360 0; loop: true; dur: 20000"  super-hands
+            geometry="primitive: sphere; radius: 1000">
+            <a-text value="{{$object->textobject}}" position="0 1 0" color="red" transparent="true"></a-text>
+        </a-entity>
+
+  <a-marker-camera preset="hiro"></a-marker-camera>
+ -->
 
     <a-scene vr-mode-ui='enabled: false' arjs='sourceType: webcam; videoTexture: true; debugUIEnabled: false'
      renderer='antialias: true; alpha: true'>
   <a-assets>
     <a-asset-item id="tree" src="{{ asset($name.'/'.$object->object) }}"></a-asset-item>
   </a-assets>
-
+  <a-camera gps-new-camera='gpsMinDistance: 5'></a-camera>
+  
   <a-entity gps-new-entity-place="latitude: {{ $location->latitude }}; longitude: {{ $location->longitude }};">
-    <a-entity id="myEntity" rotation-reader 
+    <!-- <a-entity id="myEntity" rotation-reader 
       position="0 0 -4" 
       gltf-model="#tree" animation-mixer scale="0.5 0.5 0.5"
       animation__rotate="property: rotation; to: 0 360 0; loop: true; dur: 20000"  super-hands
       geometry="primitive: sphere; radius: 1000">
       <a-text value="{{$object->textobject}}" position="0 1 0" color="red" transparent="true"></a-text>
-    </a-entity>
-
-    <a-camera gps-new-camera='gpsMinDistance: 5'></a-camera>
+    </a-entity> -->
+    <a-entity gltf-model="#tree" scale="0.5 0.5 0.5" position="0 0 -10"></a-entity>
   </a-entity>
 
   <!-- <a-marker-camera preset="hiro"></a-marker-camera> -->
