25d401bbda8fcc445a3ced8261fcc70088b45a50
fatimabed <fatima.abed@softylus.com>
2023-05-03 20:47:20 +0300

    testlocation

--- resources/views/arlocation.blade.php
+++ resources/views/arlocation.blade.php
@@ -60,14 +60,15 @@ import aframeExtrasAnimationMixer from 'https://cdn.jsdelivr.net/npm/aframe-extr
     <a-assets>
         <a-asset-item id="tree" src="{{ asset($name.'/'.$object->object) }}"></a-asset-item>
     </a-assets>
-    <a-entity  rotation-reader>
+    <a-camera gps-new-camera='gpsMinDistance: 5'></a-camera>
+    <!-- <a-entity gps-camera rotation-reader> -->
         <a-entity id="myEntity" gltf-model="#tree" animation-mixer scale="0.5 0.5 0.5" position="0 0 -4" 
             animation__rotate="property: rotation; to: 0 360 0; loop: true; dur: 20000" super-hands
             geometry="primitive: sphere; radius: 1000"
             gps-entity-place="latitude: {{ $location->latitude }}; longitude: {{ $location->longitude }}">
             <a-text value="{{$object->textobject}}" position="0 1 0" color="red" transparent="true"></a-text>
         </a-entity>
-    </a-entity>
+    <!-- </a-entity> -->
 
 
 
