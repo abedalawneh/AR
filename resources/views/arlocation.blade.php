6c44c27e6636a823c973c88fd71a44129cea1b97
fatimabed <fatima.abed@softylus.com>
2023-05-03 12:40:41 +0300

    location marker

--- resources/views/arlocation.blade.php
+++ resources/views/arlocation.blade.php
@@ -38,7 +38,7 @@ import aframeExtrasAnimationMixer from 'https://cdn.jsdelivr.net/npm/aframe-extr
 
 
 
-<a-scene embedded vr-mode-ui='enabled: false' arjs='sourceType: webcam; debugUIEnabled: false; '>
+<!-- <a-scene embedded vr-mode-ui='enabled: false' arjs='sourceType: webcam; debugUIEnabled: false; '>
   <a-assets>
     <a-asset-item id="tree" src="{{ asset($name.'/'.$object->object) }}"></a-asset-item>
   </a-assets>
@@ -52,9 +52,20 @@ import aframeExtrasAnimationMixer from 'https://cdn.jsdelivr.net/npm/aframe-extr
             <a-text value="{{$object->textobject}}" position="0 1 0" color="red" transparent="true"></a-text>
         </a-entity>
 
-  <a-marker-camera preset="hiro"></a-marker-camera>
+  <a-marker-camera preset="hiro"></a-marker-camera> -->
 
-
+  <a-scene embedded vr-mode-ui='enabled: false' arjs='sourceType: webcam; videoTexture: true; debugUIEnabled: false' renderer='antialias: true; alpha: true'>
+  <a-assets>
+    <a-asset-item id="tree" src="{{ asset($name.'/'.$object->object) }}"></a-asset-item>
+  </a-assets>  
+  <a-camera gps-new-camera='gpsMinDistance: 5'></a-camera>
+    <a-entity id="myEntity" gps-camera rotation-reader gps-entity-place="latitude: {{ $location->latitude }}; longitude: {{ $location->longitude }};"
+    position="0 0 -4" 
+    gltf-model="#tree" animation-mixer scale="0.5 0.5 0.5"
+    animation__rotate="property: rotation; to: 0 360 0; loop: true; dur: 20000"  super-hands
+            geometry="primitive: sphere; radius: 1000">
+            <a-text value="{{$object->textobject}}" position="0 1 0" color="red" transparent="true"></a-text>
+        </a-entity>
 
   <script>
   var myEntity = document.getElementById('myEntity');
