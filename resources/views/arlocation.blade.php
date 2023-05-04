f3ffe42f051fe64e597ebd9cdb28b9c4ca033aee
fatimabed <fatima.abed@softylus.com>
2023-05-04 23:53:10 +0300

    frame

--- resources/views/arlocation.blade.php
+++ resources/views/arlocation.blade.php
@@ -7,7 +7,7 @@ use App\Models\objectt;
 <html>
 
 <head>
-     <title>AR.js A-Frame Location-based</title>
+    <title>AR.js A-Frame Location-based</title>
     <script src="https://aframe.io/releases/1.0.4/aframe.min.js"></script>
     <script type='text/javascript'
         src='https://raw.githack.com/AR-js-org/AR.js/master/three.js/build/ar-threex-location-only.js'></script>
@@ -15,11 +15,9 @@ use App\Models\objectt;
     </script>
     <script src="https://cdn.jsdelivr.net/gh/donmccurdy/aframe-extras@v6.1.1/dist/aframe-extras.misc.min.js"></script>
     <script src="https://unpkg.com/super-hands@^3.0.3/dist/super-hands.min.js"></script>
-    <!-- <script src='https://aframe.io/releases/1.2.0/aframe.min.js'></script>
-  <script src='https://cdn.rawgit.com/jeromeetienne/AR.js/2.0.5/aframe/build/aframe-ar.js'></script> -->
-  <script type="module">
-import aframeExtrasAnimationMixer from 'https://cdn.jsdelivr.net/npm/aframe-extras.animation-mixer@6.1.1/+esm'
-</script>
+    <script type="module">
+    import aframeExtrasAnimationMixer from 'https://cdn.jsdelivr.net/npm/aframe-extras.animation-mixer@6.1.1/+esm'
+    </script>
 
 </head>
 
@@ -36,48 +34,19 @@ import aframeExtrasAnimationMixer from 'https://cdn.jsdelivr.net/npm/aframe-extr
               $name='glbobject';
      ?>
 
+
+
     <a-scene vr-mode-ui='enabled: false' arjs='sourceType: webcam; videoTexture: true; debugUIEnabled: false'
         renderer='antialias: true; alpha: true'>
-        <a-camera position="0 0 -4" gps-new-camera='gpsMinDistance: 5'></a-camera>
-        <a-entity  animation-mixer scale="5 5 5" gltf-model="{{ asset($name.'/'.$object->object) }}"
+        <a-camera gps-new-camera='gpsMinDistance: 5'></a-camera>
+        <a-entity position="0 0 0" scale="10 10 10" gltf-model="{{ asset($name.'/'.$object->object) }}"
             gps-new-entity-place="latitude:{{$location->latitude}}; longitude:{{ $location->longitude}}"
             animation__rotate="property: rotation; to: 0 360 0; loop: true; dur: 20000" super-hands
             geometry="primitive: sphere; radius: 1000">
-            <a-text value="{{$object->textobject}}" position="0 1 0" color="red" transparent="true"></a-text>
+            <a-text value="{{$object->textobject}}" position="0 -5 0" color="red" transparent="true"></a-text>
         </a-entity>
 
 
-
-
-        <script>
-  var myEntity = document.getElementById('myEntity');
-
-  // Load the GLB model
-  var loader = new THREE.GLTFLoader();
-  loader.load('{{ asset($name.'/'.$object->object) }}', function(glb) {
-    var objectSize = getObjectSize(glb.scene);
-    
-    var newScale = objectSize.x / 10; // Example calculation, adjust as needed
-    console.log('ttttt'+newScale);
-    if (newScale > 0.5) {
-        newScale=0.5;
-        myEntity.setAttribute('position', 0 + ' ' + 0 + ' ' + -90);
-    }
-    else if (newScale < 0.5) {
-        newScale=0.5;
-    }
-    console.log('ttttt'+newScale);
-    // Update the scale attribute of the entity element
-    myEntity.setAttribute('scale', newScale + ' ' + newScale + ' ' + newScale);
-  });
-
-  function getObjectSize(glbModel) {
-    var boundingBox = new THREE.Box3().setFromObject(glbModel);
-    var size = new THREE.Vector3();
-    boundingBox.getSize(size);
-    return size;
-  }
-</script>
 
 
         <script>
