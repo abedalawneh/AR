4a6764e09dcacc5e01c32b11ab9d4309813477e6
fatimabed <fatima.abed@softylus.com>
2023-05-04 11:56:28 +0300

    location based

--- resources/views/arlocation.blade.php
+++ resources/views/arlocation.blade.php
@@ -86,9 +86,10 @@ import aframeExtrasAnimationMixer from 'https://cdn.jsdelivr.net/npm/aframe-extr
 
     <a-scene vr-mode-ui='enabled: false' arjs='sourceType: webcam; videoTexture: true; debugUIEnabled: false' renderer='antialias: true; alpha: true'>
     <a-camera gps-new-camera='gpsMinDistance: 5'></a-camera>
-    <a-entity id="myEntity" position="0 0 -4" gps-entity-place="latitude: {{$location->latitude}}; longitude: {{$location->longitude}};"
-        gltf-model="#tree"   >
-        <a-text value="{{$object->textobject}}" position="0 1 0" color="red" transparent="true"></a-text>
+    <a-entity gps-new-entity-place="latitude: {{ $location->latitude }}; longitude: {{ $location->longitude }};" gltf-model="{{ asset($name.'/'.$object->object) }}"
+    scale="0.5 0.5 0.5"
+    rotation="0 180 0">
+    <a-text value="{{$object->textobject}}" position="0 1 0" color="red" transparent="true"></a-text>
     </a-entity>
 
 
