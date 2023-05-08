9ed5f124e83399cb4cbaf02e40677b913f8c9048
fatimabed <fatima.abed@softylus.com>
2023-05-08 14:08:35 +0300

    test1

--- resources/views/arlocation.blade.php
+++ resources/views/arlocation.blade.php
@@ -37,7 +37,7 @@ use App\Models\objectt;
 
 
 
-<a-scene embedded vr-mode-ui='enabled: false' arjs='sourceType: webcam; debugUIEnabled: false;'>
+<a-scene vr-mode-ui='enabled: false' arjs='sourceType: webcam; videoTexture: true; debugUIEnabled: false' renderer='antialias: true; alpha: true'>
     
     <a-assets>
         <a-asset-item id="tree" src="{{ asset($name.'/'.$object->object) }}"></a-asset-item>
