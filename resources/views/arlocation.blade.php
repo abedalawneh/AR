7abe095bb22732bf4571b12246abeff008e699d5
fatimabed <fatima.abed@softylus.com>
2023-05-08 10:20:09 +0300

    arlocation

--- resources/views/arlocation.blade.php
+++ resources/views/arlocation.blade.php
@@ -37,12 +37,18 @@ use App\Models\objectt;
 
 
 
-    <a-scene vr-mode-ui='enabled: false' arjs='sourceType: webcam; videoTexture: true; debugUIEnabled: false' renderer='antialias: true; alpha: true'>
-    <a-camera gps-new-camera='gpsMinDistance: 5' position="0 1.6 0" look-at="#center-object"></a-camera>
-        <a-entity id="center-object" position="0 0 -4" scale="0.5 0.5 0.5" geometry="primitive: sphere; radius: 1000"
-        animation__rotate="property: rotation; to: 0 360 0; loop: true; dur: 20000" animation-mixer super-hands
-         gltf-model="{{ asset($name.'/'.$object->object) }}"
-            gps-new-entity-place="latitude:{{$location->latitude}}; longitude:{{ $location->longitude}}">
+<a-scene embedded vr-mode-ui='enabled: false' arjs='sourceType: webcam; debugUIEnabled: false; '>
+    
+    <a-assets>
+        <a-asset-item id="tree" src="{{ asset($name.'/'.$object->object) }}"></a-asset-item>
+    </a-assets>
+    <a-marker-camera preset="hiro"></a-marker-camera>
+    
+    <a-entity id="myEntity" gps-camera rotation-reader gps-entity-place="latitude: {{ $location->latitude }}; longitude: {{ $location->longitude }};"
+    position="0 0 -4" 
+    gltf-model="#tree" animation-mixer scale="0.5 0.5 0.5"
+    animation__rotate="property: rotation; to: 0 360 0; loop: true; dur: 20000"  super-hands
+    geometry="primitive: sphere; radius: 1000">
             <a-text value="{{$object->textobject}}" position="0 1 0" color="red" transparent="true"></a-text>
     </a-entity>
     <script>
