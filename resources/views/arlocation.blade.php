7c47407da296cbf7ddaff71682dd830e8b42fd17
fatimabed <fatima.abed@softylus.com>
2023-05-09 10:40:49 +0300

    testtt

--- resources/views/arlocation.blade.php
+++ resources/views/arlocation.blade.php
@@ -9,10 +9,8 @@ use App\Models\objectt;
 <head>
     <title>AR.js A-Frame Location-based</title>
     <script src="https://aframe.io/releases/1.0.4/aframe.min.js"></script>
-    <script type='text/javascript'
-        src='https://raw.githack.com/AR-js-org/AR.js/master/three.js/build/ar-threex-location-only.js'></script>
-    <script type='text/javascript' src='https://raw.githack.com/AR-js-org/AR.js/master/aframe/build/aframe-ar.js'>
-    </script>
+    <script type='text/javascript' src='https://raw.githack.com/AR-js-org/AR.js/master/three.js/build/ar-threex-location-only.js'></script>
+    <script type='text/javascript' src='https://raw.githack.com/AR-js-org/AR.js/master/aframe/build/aframe-ar.js'></script>
     <script src="https://cdn.jsdelivr.net/gh/donmccurdy/aframe-extras@v6.1.1/dist/aframe-extras.misc.min.js"></script>
     <script src="https://unpkg.com/super-hands@^3.0.3/dist/super-hands.min.js"></script>
     <!-- <script src='https://aframe.io/releases/1.2.0/aframe.min.js'></script>
@@ -42,7 +40,6 @@ use App\Models\objectt;
 
 <a-scene vr-mode-ui='enabled: false' arjs='sourceType: webcam; videoTexture: true; debugUIEnabled: false' renderer='antialias: true; alpha: true'>
 
-    <!-- <a-camera gps-new-camera='gpsMinDistance: 5'></a-camera> -->
     <a-entity gps-camera position="0 0 -4" rotation-reader>
         <a-entity id="myEntity" gltf-model="{{ asset($name.'/'.$object->object) }}" animation-mixer scale="0.5 0.5 0.5"
         animation__rotate="property: rotation; to: 0 360 0; loop: true; dur: 20000" super-hands
