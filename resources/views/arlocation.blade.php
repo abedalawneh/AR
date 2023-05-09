207914e2f20cc64ecbfe9defdc889ab625e092e2
fatimabed <fatima.abed@softylus.com>
2023-05-09 16:03:00 +0300

    tttest

--- resources/views/arlocation.blade.php
+++ resources/views/arlocation.blade.php
@@ -10,7 +10,7 @@ use App\Models\objectt;
 <title>AR.js A-Frame Location-based</title>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/aframe/1.4.2/aframe.min.js"></script>
     <script type='text/javascript' src='https://raw.githack.com/AR-js-org/AR.js/master/three.js/build/ar-threex-location-only.js'></script>
-    <script type='text/javascript' src='https://cdnjs.cloudflare.com/ajax/libs/ar.js/2.2.2/aframe-ar.min.js'></script>
+    <script type='text/javascript' src='https://cdnjs.cloudflare.com/ajax/libs/ar.js/2.2.2/aframe-ar.js'></script>
     
 <!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/ar.js/2.2.2/aframe-ar.min.js"></script> -->
     <script src="https://cdn.jsdelivr.net/gh/donmccurdy/aframe-extras@v6.1.1/dist/aframe-extras.misc.min.js"></script>
