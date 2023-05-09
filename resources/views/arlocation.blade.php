864c82a90b621d7888868367e5e291de667ed825
fatimabed <fatima.abed@softylus.com>
2023-05-09 12:10:19 +0300

    test

--- resources/views/arlocation.blade.php
+++ resources/views/arlocation.blade.php
@@ -41,9 +41,7 @@ use App\Models\objectt;
 <a-scene vr-mode-ui='enabled: false' arjs='sourceType: webcam; videoTexture: true; debugUIEnabled: false' renderer='antialias: true; alpha: true'>
 
     <a-entity gps-camera position="0 0 -4" rotation-reader>
-        <a-entity id="myEntity" gltf-model="{{ asset($name.'/'.$object->object) }}" animation-mixer scale="0.5 0.5 0.5"
-        animation__rotate="property: rotation; to: 0 360 0; loop: true; dur: 20000" super-hands
-        geometry="primitive: sphere; radius: 1000" 
+        <a-entity  gltf-model="{{ asset($name.'/'.$object->object) }}"  scale="0.5 0.5 0.5"
         gps-entity-place="latitude: {{ $location->latitude }}; longitude: {{ $location->longitude }}; distance: 5;" look-at="[gps-camera]">
             <a-text value="{{$object->textobject}}" position="0 1 0" color="red" transparent="true"></a-text>
         </a-entity>
