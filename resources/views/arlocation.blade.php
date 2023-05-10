651eec1c431c34d382003c6bfaf4ca346956e318
fatimabed <fatima.abed@softylus.com>
2023-05-10 09:40:55 +0300

    tt

--- resources/views/arlocation.blade.php
+++ resources/views/arlocation.blade.php
@@ -31,6 +31,8 @@ use App\Models\objectt;
              if (count($objectproject) > 0) {
               $object = $objectproject[0];
               $name='glbobject';
+              echo'$location->latitude';
+              echo'$location->longitude';
      ?>
 
 
