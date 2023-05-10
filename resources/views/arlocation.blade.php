25fdefce47d6ab23b2d1d5a9d172eadbc5cc0a18
fatimabed <fatima.abed@softylus.com>
2023-05-10 10:26:23 +0300

    version

--- resources/views/arlocation.blade.php
+++ resources/views/arlocation.blade.php
@@ -9,14 +9,14 @@ use App\Models\objectt;
 <head>
 <title>AR.js A-Frame Location-based</title>
 
-<script src="https://aframe.io/releases/1.0.4/aframe.min.js"></script>
+<script src="https://aframe.io/releases/1.2.0/aframe.min.js"></script>
 <script type='text/javascript' src='https://raw.githack.com/AR-js-org/AR.js/master/three.js/build/ar-threex-location-only.js'></script>
-<script type='text/javascript' src='https://cdnjs.cloudflare.com/ajax/libs/ar.js/2.2.2/aframe-ar.js'></script>
-<!-- <script src="https://cdn.jsdelivr.net/gh/donmccurdy/aframe-extras@v6.1.1/dist/aframe-extras.misc.min.js"></script> -->
-<script src="https://unpkg.com/super-hands@^3.0.3/dist/super-hands.min.js"></script>
+<script type='text/javascript' src='https://cdnjs.cloudflare.com/ajax/libs/ar.js/3.1.2/aframe-ar.js'></script>
+<script src="https://unpkg.com/super-hands@^4.0.1/dist/super-hands.min.js"></script>
 <script type="module">
-  import aframeExtrasAnimationMixer from 'https://cdn.jsdelivr.net/npm/aframe-extras.animation-mixer@6.1.1/+esm'
+  import aframeExtrasAnimationMixer from 'https://cdn.jsdelivr.net/npm/aframe-extras.animation-mixer@6.2.0/+esm'
 </script>
+
 
 <body>
     <?php
