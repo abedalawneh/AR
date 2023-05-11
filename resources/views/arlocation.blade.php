dee6980c28c96d276fce1451ed6074992888cb4e
fatimabed <fatima.abed@softylus.com>
2023-05-11 16:36:30 +0300

    112

--- resources/views/arlocation.blade.php
+++ resources/views/arlocation.blade.php
@@ -14,7 +14,7 @@ use App\Models\objectt;
     <script src="https://unpkg.com/aframe-look-at-component@0.8.0/dist/aframe-look-at-component.min.js"></script>
     <script src="https://raw.githack.com/AR-js-org/AR.js/master/aframe/build/aframe-ar-nft.js"></script>
     <script src="https://cdn.rawgit.com/donmccurdy/aframe-extras/v6.0.0/dist/aframe-extras.min.js"></script>
-    <!-- <script src="https://cdn.jsdelivr.net/npm/super-hands/dist/super-hands.min.js"></script> -->
+    <script src="https://cdn.jsdelivr.net/npm/super-hands/dist/super-hands.min.js"></script>
 
 
 </head>
@@ -37,15 +37,16 @@ use App\Models\objectt;
     >
       <a-entity
         gltf-model="{{ asset($name.'/'.$object->object) }}"
-        value="{{$object->textobject}}"
         scale="3 3 3"
         position="0 0 -4"
         look-at="[gps-camera]"
         gps-entity-place="latitude: {{ $location->latitude }}; longitude: {{ $location->longitude }};"
         animation__rotate="property: rotation; to: 0 360 0; loop: true; dur: 10000"
         animation-mixer
-      ></a-entity>
-      <a-camera gps-camera rotation-reader  animation-mixer></a-camera>
+      >
+      <a-text value="{{$object->textobject}}" position="0 -5 0" color="red" transparent="true"></a-text>
+        </a-entity>
+      <a-camera gps-camera rotation-reader super-hands animation-mixer></a-camera>
 
 <script>
         
