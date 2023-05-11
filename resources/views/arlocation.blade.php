a9eb39d69dfd03040c27f0ee58ae3bd26ae7b1b7
fatimabed <fatima.abed@softylus.com>
2023-05-11 10:57:30 +0300

    39

--- resources/views/arlocation.blade.php
+++ resources/views/arlocation.blade.php
@@ -15,7 +15,7 @@ use App\Models\objectt;
     <script src="https://raw.githack.com/AR-js-org/AR.js/master/aframe/build/aframe-ar-nft.js"></script>
     <script src="https://cdn.jsdelivr.net/gh/donmccurdy/aframe-extras@v6.1.1/dist/aframe-extras.misc.min.js"></script>
     <script src="https://unpkg.com/super-hands@^3.0.3/dist/super-hands.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/aframe-extras.animation-mixer@6.1.1/dist/aframe-extras.animation-mixer.min.js"></script>
+    <!-- <script src="https://cdn.jsdelivr.net/npm/aframe-extras.animation-mixer@6.1.1/dist/aframe-extras.animation-mixer.min.js"></script> -->
 
 </head>
 <body>
