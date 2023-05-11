4222f168518f3d2469ee0d5d85320c5073f6bb02
fatimabed <fatima.abed@softylus.com>
2023-05-11 12:54:16 +0300

    63

--- resources/views/arlocation.blade.php
+++ resources/views/arlocation.blade.php
@@ -10,25 +10,14 @@ use App\Models\objectt;
 <meta charset="utf-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <title>GeoAR.js demo</title>
-    <script src="https://aframe.io/releases/1.0.4/aframe.min.js"></script>
+    <script src="https://aframe.io/releases/1.3.0/aframe.min.js"></script>
     <script src="https://unpkg.com/aframe-look-at-component@0.8.0/dist/aframe-look-at-component.min.js"></script>
     <script src="https://raw.githack.com/AR-js-org/AR.js/master/aframe/build/aframe-ar-nft.js"></script>
     <!-- <script src="https://cdn.jsdelivr.net/gh/donmccurdy/aframe-extras@v6.1.1/dist/aframe-extras.misc.min.js"></script> -->
     <script src="https://unpkg.com/super-hands@^3.0.3/dist/super-hands.min.js"></script>
-<script src="https://cdn.jsdelivr.net/gh/donmccurdy/aframe-extras@v6.1.1/dist/aframe-extras.min.js"></script>
-<script>
-  function pause() {
-    document.querySelector("a-entity").setAttribute('animation-mixer', {
-      timeScale: 0
-    });
-  }
-
-  function play() {
-    document.querySelector("a-entity").setAttribute('animation-mixer', {
-      timeScale: 1
-    });
-  }
-</script>
+    <script type="module">
+    import aframeExtrasAnimationMixer from 'https://cdn.jsdelivr.net/npm/aframe-extras.animation-mixer@6.1.1/+esm'
+    </script>
 
    
 </head>
