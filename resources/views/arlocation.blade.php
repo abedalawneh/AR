b1a485c2acdb06a91c8c59df4dcb4d384f7d3610
fatimabed <fatima.abed@softylus.com>
2023-05-11 10:17:01 +0300

    26

--- resources/views/arlocation.blade.php
+++ resources/views/arlocation.blade.php
@@ -7,13 +7,18 @@ use App\Models\objectt;
 <html>
 
 <head?>
-<meta charset="utf-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <title>GeoAR.js demo</title>
-    <script src="https://aframe.io/releases/1.3.0/aframe.min.js"></script>
-    <script src="https://unpkg.com/aframe-look-at-component@0.8.0/dist/aframe-look-at-component.min.js"></script>
-    <script src="https://raw.githack.com/AR-js-org/AR.js/master/aframe/build/aframe-ar-nft.js"></script>
-    <script src="https://cdn.jsdelivr.net/gh/donmccurdy/aframe-extras@v6.1.1/dist/aframe-extras.misc.min.js"></script>
+<title>AR.js A-Frame Location-based</title>
+
+<!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/aframe/1.4.2/aframe.min.js"></script>
+<script type='text/javascript' src='https://cdnjs.cloudflare.com/ajax/libs/ar.js/2.2.2/aframe-ar.min.js'></script> -->
+<!-- <script type='text/javascript' src='https://raw.githack.com/AR-js-org/AR.js/master/three.js/build/ar-threex-location-only.js'></script> -->
+<!-- <script src="https://aframe.io/releases/1.0.4/aframe.min.js"></script> -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/aframe/1.4.2/aframe.min.js"></script>
+<script type='text/javascript'src='https://raw.githack.com/AR-js-org/AR.js/master/three.js/build/ar-threex-location-only.js'></script>
+<script type='text/javascript' src='https://raw.githack.com/AR-js-org/AR.js/master/aframe/build/aframe-ar.js'></script>
+<script type='text/javascript' src='https://cdnjs.cloudflare.com/ajax/libs/ar.js/2.2.2/aframe-ar.min.js'></script>
+
+<script src="https://cdn.jsdelivr.net/gh/donmccurdy/aframe-extras@v6.1.1/dist/aframe-extras.misc.min.js"></script>
 <script src="https://unpkg.com/super-hands@^3.0.3/dist/super-hands.min.js"></script>
 <script type="module">
 import aframeExtrasAnimationMixer from 'https://cdn.jsdelivr.net/npm/aframe-extras.animation-mixer@6.1.1/+esm'
@@ -32,23 +37,16 @@ import aframeExtrasAnimationMixer from 'https://cdn.jsdelivr.net/npm/aframe-extr
               $name='glbobject';
               
      ?>
+<a-scene vr-mode-ui='enabled: false' arjs='sourceType: webcam; videoTexture: true; debugUIEnabled: false' renderer='antialias: true; alpha: true'>
 
-    <a-scene
-      vr-mode-ui="enabled: false"
-      arjs="sourceType: webcam; videoTexture: true; debugUIEnabled: false;"
-    >
-      <a-text
-      gltf-model="{{ asset($name.'/'.$object->object) }}"
-      value="{{$object->textobject}}"
-      scale="0.5 0.5 0.5"
-        look-at="[gps-camera]"
-        gps-entity-place="latitude:{{ $location->latitude }}; longitude: {{ $location->longitude }};"
-        animation__rotate="property: rotation; to: 0 360 0; loop: true; dur: 20000"
-         super-hands
-         animation-mixer
+    <a-entity gps-camera position="0 0 -4" rotation-reader>
+        <a-entity  gltf-model="{{ asset($name.'/'.$object->object) }}" animation-mixer scale="0.5 0.5 0.5"
+        animation__rotate="property: rotation; to: 0 360 0; loop: true; dur: 20000" super-hands
         geometry="primitive: sphere; radius: 1000" 
-      ></a-text>
-      <a-camera gps-camera rotation-reader> </a-camera>
+        gps-entity-place="latitude: {{ $location->latitude }}; longitude: {{ $location->longitude }};" >
+            <a-text value="{{$object->textobject}}" position="0 1 0" color="red" transparent="true"></a-text>
+        </a-entity>
+    </a-entity>
 
     <script>
         // var animation='{{$object->animation}}';
