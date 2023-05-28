458e05b99242a81f48edc6b273720e65e88617b7
fatimabed <fatima.abed@softylus.com>
2023-05-28 12:39:56 +0300

    scale10

--- resources/views/arlocation.blade.php
+++ resources/views/arlocation.blade.php
@@ -61,11 +61,11 @@ use App\Models\objectt;
     var newScale = objectSize.x / 10; // Example calculation, adjust as needed
     console.log('ttttt'+newScale);
     if (newScale >1) {
-        newScale=4;
+        newScale=10;
         myEntity.setAttribute('position', 0 + ' ' + 0 + ' ' + -90);
     }
     else if (newScale < 1) {
-        newScale=4;
+        newScale=10;
     }
     console.log('ttttt'+newScale);
     // Update the scale attribute of the entity element
